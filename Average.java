f21e8b0c68067ad634441206b70c75b654e9d50d
Hiteshgupta20 <67308966+Hiteshgupta20@users.noreply.github.com>
2020-07-02 19:45:14 +0530

    BASIC JAVA

---
+++ Average.java
@@ -0,0 +1,16 @@
+
+public class Average {
+
+	public static void main(String[] args) {
+	
+		StudentMarks Student1 = new StudentMarks("hitesh",5, 353);
+		StudentMarks Student2= new StudentMarks("rajat",5,350);
+		
+		Student1.Students();
+		Student1.Average();
+		Student2.Students();
+		Student2.Average();
+
+	}
+
+}
