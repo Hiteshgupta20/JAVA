83bdc91c01077a00730714cfb70277d61eea035d
Hiteshgupta20 <67308966+Hiteshgupta20@users.noreply.github.com>
2020-07-04 13:36:01 +0530

    BASIC JAVA

--- Average.java
+++ Average.java
@@ -1,15 +1,29 @@
-
-public class Average {
+/*PROGRAM TO FIND AVERAGE OF 3 NUMBERS
+ * 1.INPUT 3 NUMBERS FROM USER AND STORE IT AS INT
+ * 2.USING AVERAGE :(num1 + num2 + num3)/3;
+ * 3.PRINT AVERAGE OF 3 NUMBERS
+ */
+package basic1;
+import java.util.*;
+public class average {
 
 	public static void main(String[] args) {
 	
-		StudentMarks Student1 = new StudentMarks("hitesh",5, 353);
-		StudentMarks Student2= new StudentMarks("rajat",5,350);
+		Scanner sc=new Scanner(System.in);
+		//INPUT THE NUMBER1
+		System.out.println("enter the number 1");
+		int num1=sc.nextInt();
+		//INPUT 2ND NUMBER
+		System.out.println("enter the number 2");
+		int num2 = sc.nextInt();
+		//INPUT 3RD NUMBER
+		System.out.println("enter the number 3");
+		int num3 = sc.nextInt();
+		// AVERAGE 
+		int avg = (num1 + num2 + num3)/3;
+		System.out.println("Average of three numbers : " +avg );						
 		
-		Student1.Students();
-		Student1.Average();
-		Student2.Students();
-		Student2.Average();
+		
 
 	}
 
