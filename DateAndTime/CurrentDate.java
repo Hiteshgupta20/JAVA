f67c8734f32bab12d19ae6f5c5b2f923657d000f
Hiteshgupta20 <67308966+Hiteshgupta20@users.noreply.github.com>
2020-07-17 12:23:14 +0530

    DATE AND TIME

---
+++ DateAndTime/CurrentDate.java
@@ -0,0 +1,12 @@
+package DateAndTime;
+import java.util.*;
+public class CurrentDate {
+
+	public static void main(String[] args) {
+	
+		Date d1=new Date();
+		System.out.println("Current date is :" +d1);
+
+	}
+
+}
