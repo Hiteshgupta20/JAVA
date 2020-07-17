f0a68c6bf2fe22c4ef38b475e83e768cf2352889
Hiteshgupta20 <67308966+Hiteshgupta20@users.noreply.github.com>
2020-07-17 12:39:01 +0530

    DATE AND TIME

--- DateAndTime/CurrentDate.java
+++ DateAndTime/CurrentDate.java
@@ -1,9 +1,10 @@
+/// Java program to print current date
 package DateAndTime;
 import java.util.*;
 public class CurrentDate {
 
 	public static void main(String[] args) {
-	
+		//time using Date class.
 		Date d1=new Date();
 		System.out.println("Current date is :" +d1);
 
---
+++ DateAndTime/DateAndTimeUsingCalenderClass.java
@@ -0,0 +1,29 @@
+// Java program to print current date
+package DateAndTime;
+import java.time.MonthDay;
+import java.util.Calendar;
+
+public class DateAndTimeUsingCalenderClass {
+
+	public static void main(String[] args) {
+		//time using calender class
+		Calendar c=Calendar.getInstance();
+		
+		//print date and time 
+		System.out.println("WEEK IN MONTH : "+c.get(Calendar.WEEK_OF_MONTH));
+		System.out.println("WEEK IN YEAR : "+c.get(Calendar.WEEK_OF_YEAR));
+		System.out.println("DAY OF MONTH : "+c.get(Calendar.DAY_OF_MONTH));
+		System.out.println("DAY OF WEEK IN MONTH : "+c.get(Calendar.DAY_OF_WEEK_IN_MONTH));
+		System.out.println("DAY OF WEEK : " +c.get(Calendar.DAY_OF_WEEK));
+		System.out.println("DAY OF YEAR : " +c.get(Calendar.DAY_OF_YEAR));
+		System.out.println("HOUR : "+c.get(Calendar.HOUR));
+		System.out.println("MINUTE :"+c.get(Calendar.MINUTE));
+		System.out.println("SECOND :"+c.get(Calendar.SECOND));
+		System.out.println("AM OR PM :"+c.get(Calendar.AM_PM));
+		
+		
+		
+
+	}
+
+}
